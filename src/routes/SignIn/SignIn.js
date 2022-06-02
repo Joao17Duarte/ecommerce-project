0c02c41e156032813ccf0e8f5d5088306fef7762
Joao Duarte <joaopd17@hotmail.com>
2022-06-02 20:01:31 +0200

    Add sign in file to routes folder

--- src/routes/SignIn/SignIn.js
+++ src/routes/SignIn/SignIn.js
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup } from '../../utils/Firebase/Firebase'
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup()
+    console.log(response)
+  }
   return (
     <div>
       <h1>SignIn Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   )
 }
