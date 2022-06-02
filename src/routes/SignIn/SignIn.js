3ae72e5181ff61eb7997034f4606855a98026793
Joao Duarte <joaopd17@hotmail.com>
2022-06-02 20:49:59 +0200

    Add logic to sign in with google prompt and store it in firestore DB

--- src/routes/SignIn/SignIn.js
+++ src/routes/SignIn/SignIn.js
@@ -1,16 +1,19 @@
-import { signInWithGooglePopup } from '../../utils/Firebase/Firebase'
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/Firebase/Firebase'
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup()
-    console.log(response)
+    const { user } = await signInWithGooglePopup()
+    const userDocRef = await createUserDocumentFromAuth(user)
   }
+
   return (
     <div>
-      <h1>SignIn Page</h1>
+      <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   )
 }
-
 export default SignIn
